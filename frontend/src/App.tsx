844423fc3c73713ba3a0e7f6c67ab9535f78beee
allanc3500 <allanc3500@gmail.com>
2025-03-17 10:44:35 -0500

    add tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,9 +18,6 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <Link to="/landing-page">Go to JourneyJot</Link>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- frontend/src/pages/landing-page.tsx
+++ frontend/src/pages/landing-page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage: React.FC = () => {
-  return <div>WELCOME TO JOURNEY JOT</div>;
+  return (
+    <>
+      <div className="text-9xl">WELCOME TO JOURNEY JOT</div>
+      <Link to="/">Go back to Homepage</Link>
+    </>
+  );
 };
 
 export default LandingPage;
